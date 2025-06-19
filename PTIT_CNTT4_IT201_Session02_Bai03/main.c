5bff472af2607fe8ad8912307a1f3c1ca1a69685
Nguyễn Doanh Tuấn <tuan20062012@gmail.com>
2025-06-20 01:54:41 +0700

    Update main.c

--- PTIT_CNTT4_IT201_Session02_Bai03/main.c
+++ PTIT_CNTT4_IT201_Session02_Bai03/main.c
@@ -16,12 +16,13 @@ int main(void) {
     return 0;
 }
  void reverseArray(int *arr, int size) {
-    for(int i=0;i<(size-1)/2;i++) {
+    for (int i = 0; i < size / 2; i++) {
         int temp = arr[i];
-        arr[i] = arr[size-2-i];
-        arr[size-2-i] = temp;
+        arr[i] = arr[size - 1 - i];
+        arr[size - 1 - i] = temp;
     }
-    for(int i=0;i<size-1;i++) {
-        printf("%d \t  ", arr[i]);
+
+    for (int i = 0; i < size; i++) {
+        printf("%d \t", arr[i]);
     }
 }
